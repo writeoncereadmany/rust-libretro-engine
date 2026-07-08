822bff7eb96d134e7572c75a3c88f21b7ce5607a
Tom Johnson <tarjohnson1979@gmail.com>
2026-07-08 11:35:23 +0300

    parse args using clap in export assets

--- src/export.rs
+++ src/export.rs
@@ -1,14 +1,25 @@
-use std::env;
 use std::fs::File;
 use std::io::Write;
+use clap::Parser;
 use crate::assets::Assets;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None, ignore_errors = true)]
+pub struct Args {
+    /// The directory in which to find assets
+    #[arg(short, long)]
+    pub source: String,
+    /// The filename for the finished assets file
+    #[arg(short, long)]
+    pub output: String
+}
+
 pub fn export_assets() -> std::io::Result<()> {
-    let args = env::args().collect::<Vec<String>>();
-    let mut file_output = File::create(args.get(2).expect("No output file specified"))?;
+    let args = Args::parse();
+    let mut file_output = File::create(&args.output)?;
 
     let mut assets = Assets::new();
-    assets.load_from_filesystem("assets");
+    assets.load_from_filesystem(&args.source);
 
     let binary_data = serde_json::to_vec(&assets)?;
     file_output.write(binary_data.as_slice())?;
